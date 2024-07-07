d5348158fb5d7e6edb95b2d5c63ccc9e5e4f0734
StanKrajci108 <krajci.s@tutanota.com>
2024-07-07 23:26:29 +0200

    commit

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const icons = document.querySelectorAll(".icon");
 // Add click event listeners to each icon
 icons.forEach((icon) => {
   icon.addEventListener("click", function (event) {
+    // Check if the clicked target is the icon itself
+    if (event.target.closest(".contact-infoMail .email-link")) {
+      // Do nothing if the click is on the email link
+      return;
+    }
     const contactInfo = icon.querySelector(
       ".contact-infoPhone, .contact-infoMail"
     );
-    if (contactInfo && !event.target.closest(".contact-infoMail span")) {
+    if (contactInfo) {
       event.preventDefault();
       // Toggle active class on clicked icon
       icon.classList.toggle("active");
@@ -23,13 +28,15 @@ icons.forEach((icon) => {
 
 // Handle click events for the email link and phone numbers specifically
 document.addEventListener("click", function (event) {
-  const emailLink = event.target.closest(".contact-infoMail span");
+  const emailLink = event.target.closest(".contact-infoMail .email-link");
   if (emailLink) {
     window.location.href = "mailto:" + emailLink.textContent;
+    event.preventDefault();
   }
 
   const phoneNumber = event.target.closest(".phone-number span");
   if (phoneNumber) {
     window.location.href = "tel:" + phoneNumber.textContent;
+    event.preventDefault();
   }
 });
